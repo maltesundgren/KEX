2d278cf0134d5c03309aff86672f7374de804cca
mhellmin <123269253+mhellmin@users.noreply.github.com>
2024-04-29 13:48:50 +0200

    gjorde simulering av cost of resources

--- example_world3_standard.py
+++ example_world3_standard.py
@@ -8,13 +8,24 @@ from pyworld3.utils import plot_world_variables
 params = {"lines.linewidth": "3"}
 plt.rcParams.update(params)
 
-world3 = World3(year_max=2500)
+world3 = World3(year_max=2200)
 world3.set_world3_control()
 world3.init_world3_constants()
 world3.init_world3_variables()
 world3.set_world3_table_functions()
 world3.set_world3_delay_functions()
 world3.run_world3(fast=False)
+
+plot_world_variables(
+    world3.time,
+    [world3.nrfr, world3.iopc, world3.fcaor,],
+    ["NRFR\n[]", "IOPC\n[$/py]", "FCAOR\n[]"],
+    [[0, 1.05], [0,0.6e3], [0,1.05]],
+    #img_background="./img/fig7-7.png",
+    figsize=(7, 5),
+    #title="World3 standard run - POP",
+)
+plt.savefig("fig_world3_standard_Cost_of_Resources.pdf")
 
 """
 plot_world_variables(
